ec552d6ba918e784957192b17596fc1f7f6253d1
Jose Manuel Palomo Zambrano <palomo.zambrano.jose.manuel@iescamas.es>
2025-02-25 17:50:09 +0100

    eleleel

--- src/Ej21/AppClub.java
+++ src/Ej21/AppClub.java
@@ -1,8 +1,6 @@
 package Ej21;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class AppClub {
 
@@ -18,20 +16,84 @@ public class AppClub {
 			MostrarMenu();
 			opcion = sc.nextInt();
 			sc.nextLine();
-			
-			
+
 			switch (opcion) {
 
 			case 1:
 
+				System.out.println("ingrese el DNI: ");
+				int dni = sc.nextInt();
+				sc.nextLine();
+
+				if (mapa.containsKey(dni)) {
+
+					System.out.println("EL dni ya esta en el mapa ");
+					sc.nextLine();
+
+				} else {
+
+					System.out.println("ingrese su nombre: ");
+					String nombre = sc.nextLine();
+
+					System.out.println("ingrese fecha de entrada (año) ");
+					int fecha = sc.nextInt();
+					sc.nextLine();
+
+					mapa.put(dni, new Club(nombre, fecha));
+
+				}
+
 				break;
 			case 2:
+				System.out.println("inserte el DNI que desea eliminar");
+
+				int dniBorrar = sc.nextInt();
+				sc.nextLine();
+
+				if (!mapa.containsKey(dniBorrar)) {
+
+					System.out.println("el dni no se encuentra ");
+
+				} else {
+
+					mapa.remove(dniBorrar);
+
+				}
 
 				break;
 			case 3:
 
+				System.out.println("Inserte el DNI cuyos valores quiere modificar ");
+
+				int dniCambiar = sc.nextInt();
+				sc.nextLine();
+
+				if (!mapa.containsKey(dniCambiar)) {
+
+					System.out.println("el dni no existe");
+					return;
+				} else {
+
+					System.out.println("introduzca el nuevo nombre ");
+
+					String NombreNuevo = sc.nextLine();
+
+					System.out.println("introduzca la nueva fecha ");
+
+					int FechaNueva = sc.nextInt();
+					sc.nextLine();
+
+					
+					mapa.put(dniCambiar, new Club(NombreNuevo, FechaNueva));
+
+					
+					
+
+				}
+
 				break;
 			case 4:
+				listarPorDNI(mapa);
 
 				break;
 			case 5:
@@ -43,6 +105,7 @@ public class AppClub {
 
 			default:
 				System.out.println("numero no valido ");
+				break;
 
 			}
 		} while (opcion != 6);
@@ -53,12 +116,27 @@ public class AppClub {
 	private static void MostrarMenu() {
 		System.out.println("elija que hacer ");
 		System.out.println("1.Alta ");
-		System.out.println("2 Baja ");
-		System.out.println("3 Modificacion ");
-		System.out.println("4 Listado por DNI ");
-		System.out.println("5 Listado por antiguedad ");
-		System.out.println("6 Salir ");
+		System.out.println("2.Baja ");
+		System.out.println("3.Modificacion ");
+		System.out.println("4.Listado por DNI ");
+		System.out.println("5.Listado por antiguedad ");
+		System.out.println("6.Salir ");
 
+	}
+
+	private static void listarPorDNI(Map<Integer, Club> mapa) {
+		if (mapa.isEmpty()) {
+			System.out.println("No hay clubes registrados.");
+			return;
+		}
+
+		List<Integer> dnis = new ArrayList<>(mapa.keySet()); // Obtener DNIs
+		Collections.sort(dnis); // Ordenar DNIs
+
+		System.out.println("\nListado de clubes por DNI:");
+		for (int dni : dnis) {
+			System.out.println("DNI: " + dni + " -> " + mapa.get(dni));
+		}
 	}
 
 }
